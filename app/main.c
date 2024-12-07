41457a32a4bfb1d901299902704de16ac48560b1
ruturajsawant5 <ruturajsawant5@gmail.com>
2024-12-08 04:09:30 +0530

    codecrafters submit [skip ci]

--- app/main.c
+++ app/main.c
@@ -1,11 +1,21 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 int main() {
+  char* cmd = NULL;
   printf("$ ");
   fflush(stdout);
 
   // Wait for user input
   char input[100];
   fgets(input, 100, stdin);
+
+  //TODO: remove newline at end
+  input[strlen(input) - 1] = '\0';
+
+  cmd = strtok(input, " ");
+  printf("%s: command not found\n", cmd);
+  
   return 0;
 }
